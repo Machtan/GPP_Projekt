1a6b03732e935c65d16d7a2814c67fbfbe5b15df
patr0805 <puritymail@gmail.com>
2013-12-12 21:34:03 +0100

    FlightBrowser & ReservationEditor
    *add the library swingx-0.9.2 from the project folder to the project.

--- GPP_Flyprojekt/src/classes/FlightBrowser.java
+++ GPP_Flyprojekt/src/classes/FlightBrowser.java
@@ -8,15 +8,18 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.text.NumberFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Iterator;
 import javax.swing.JPanel;
 import javax.swing.table.DefaultTableModel;
+import org.jdesktop.swingx.JXDatePicker;
 
 /**
- * FlightBrowser is the top class for a flight browser, which lets the user 
- * browse flights in a table view, search for specific flights and select a 
+ * FlightBrowser is the top class for a flight browser, which lets the user
+ * browse flights in a table view, search for specific flights and select a
  * specific flight.
+ *
  * @author Patrick Evers Bjørkman
  */
 public class FlightBrowser extends Browser {
@@ -57,9 +60,10 @@ public class FlightBrowser extends Browser {
         }
         UpdateTable();
     }
-    
+
     /**
      * Returns the currently selected Flight
+     *
      * @return The chosen flight or null if nothing is selected
      */
     public Flight getChosen() {
@@ -77,6 +81,14 @@ public class FlightBrowser extends Browser {
                 model.removeRow(i);
             }
         }
+
+                int arrivalHour = searchArrivalTimeHourTextField.GetIntegerValue();
+                int arrivalMinute = searchArrivalTimeMinuteTextField.GetIntegerValue();
+                int departureHour = searchDepartureTimeHourTextField.GetIntegerValue();
+                int departureMinute = searchDepartureTimeMinuteTextField.GetIntegerValue();
+                
+                
+        Calendar calendar = Calendar.getInstance();
         for (Flight flight : flights) {
             if ((flight.getID() != "" && !flight.getID().toLowerCase().contains(searchFlightIDTextField.getText()))) {
                 continue;
@@ -87,23 +99,55 @@ public class FlightBrowser extends Browser {
             if ((searchDestinationComboBox.getSelectedIndex() != 0 && !flight.getDestination().contains((String) searchDestinationComboBox.getSelectedItem()))) {
                 continue;
             }
-            if ((searchDepartureTextField.getText() != "" && !flight.getDepartureTime().toString().contains(searchDepartureTextField.getText()))) {
-                continue;
+            if (searchDepartureJXDatePicker.getDate() != null) {
+                calendar.setTime(searchDepartureJXDatePicker.getDate());
+                calendar.set(Calendar.MINUTE, departureMinute);
+                calendar.set(Calendar.HOUR_OF_DAY, departureHour);
+                if (calendar.getTime().compareTo(flight.getDepartureTime()) > 0) {
+                    continue;
+                }
+
+
+            }else if (searchDepartureTimeHourTextField.GetIntegerValue() > 0 || searchDepartureTimeHourTextField.GetIntegerValue() > 0) {
+                calendar.setTime(flight.getDepartureTime());
+                calendar.set(Calendar.MINUTE, departureMinute);
+                calendar.set(Calendar.HOUR_OF_DAY, departureHour);
+                if (calendar.getTime().compareTo(flight.getDepartureTime()) > 0) {
+                    continue;
+                }
+
             }
-            if ((searchArrivalTextField.getText() != "" && !flight.getArrivalTime().toString().contains(searchArrivalTextField.getText()))) {
-                continue;
+
+            if (searchArrivalJXDatePicker.getDate() != null) {
+                calendar.setTime(searchArrivalJXDatePicker.getDate());
+                calendar.set(Calendar.MINUTE, arrivalMinute);
+                calendar.set(Calendar.HOUR_OF_DAY, arrivalHour);
+                if (calendar.getTime().compareTo(flight.getArrivalTime()) < 0) {
+                    continue;
+                }
+
+            } else if (searchArrivalTimeHourTextField.GetIntegerValue() > 0 || searchArrivalTimeHourTextField.GetIntegerValue() > 0) {
+                calendar.setTime(flight.getArrivalTime());
+                calendar.set(Calendar.MINUTE, arrivalMinute);
+                calendar.set(Calendar.HOUR_OF_DAY, arrivalHour);
+                if (calendar.getTime().compareTo(flight.getArrivalTime()) < 0) {
+                    continue;
+                }
+
             }
 
             //Get number of avavlible seats
             ISeating seating = new Seating(flight);
             Iterator<Point> seats = seating.getSeatIterator();
             int numberOfFreeSeats = seating.getNumberOfFreeSeats();
-            
-            String departureTime = flight.getDepartureTime().toString();
-            String arrivalTime = flight.getArrivalTime().toString();
-            
-            model.addRow(new Object[]{flight.id, flight.getOrigin(), 
-                flight.getDestination(), departureTime, arrivalTime, numberOfFreeSeats});
+            calendar.setTime(flight.getDepartureTime());
+           
+            String departureDate = (calendar.get(Calendar.DAY_OF_MONTH) + "-" + (calendar.get(Calendar.MONTH)+1) + "-" + calendar.get(Calendar.YEAR) + " " + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE));
+            calendar.setTime(flight.getArrivalTime());
+            String arrivalDate = (calendar.get(Calendar.DAY_OF_MONTH) + "-" +(calendar.get(Calendar.MONTH)+1) + "-" + calendar.get(Calendar.YEAR) + " " + calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE));
+
+            model.addRow(new Object[]{flight.id, flight.getOrigin(),
+                flight.getDestination(), departureDate, arrivalDate, numberOfFreeSeats});
 
         }
     }
@@ -112,7 +156,7 @@ public class FlightBrowser extends Browser {
         setTitle("Vælg en afgang");
         setName("flightFrame"); // NOI18N
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setMinimumSize(new Dimension(950, 600));
+        setMinimumSize(new Dimension(1045, 600));
         searchPanel = new javax.swing.JPanel();
         searchFlightIDTextField = new javax.swing.JTextField();
         searchFlightIDLabel = new javax.swing.JLabel();
@@ -121,10 +165,18 @@ public class FlightBrowser extends Browser {
         searchOriginComboBox = new javax.swing.JComboBox();
         searchDestinationLabel = new javax.swing.JLabel();
         searchDestinationComboBox = new javax.swing.JComboBox();
-        searchDepartureTextField = new javax.swing.JTextField();
         searchDepartureDLabel = new javax.swing.JLabel();
         searchArrivalLabel = new javax.swing.JLabel();
-        searchArrivalTextField = new javax.swing.JTextField();
+        searchArrivalJXDatePicker = new JXDatePicker();
+        searchDepartureJXDatePicker = new JXDatePicker();
+        searchArrivalTimeHourTextField = new external.HintTextField("Time");
+        searchArrivalTimeMinuteTextField = new external.HintTextField("Minut");
+        searchDepartureTimeHourTextField = new external.HintTextField("Time");
+        searchDepartureTimeMinuteTextField = new external.HintTextField("Minut");
+        searchTimeSplitLetterDepartureLabel = new javax.swing.JLabel();
+        searchTimeSplitLetterArrivalLabel = new javax.swing.JLabel();
+        searchArrivalTimeLabel = new javax.swing.JLabel();
+        searchDepartureTimeLabel = new javax.swing.JLabel();
         searchMinSeatsLabel = new javax.swing.JLabel();
         searchMinSeatsTextField = new javax.swing.JFormattedTextField(NumberFormat.getNumberInstance());
         clearSearchButton = new javax.swing.JButton();
@@ -137,61 +189,78 @@ public class FlightBrowser extends Browser {
         searchPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Søg"));
         searchPanel.setToolTipText("");
         searchPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        searchPanel.add(searchFlightIDTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(82, 18, 264, -1));
+        searchPanel.add(searchFlightIDTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(102, 18, 204, 25));
 
         searchFlightIDLabel.setText("ID");
-        searchPanel.add(searchFlightIDLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 21, -1, -1));
+        searchPanel.add(searchFlightIDLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 18, -1, 25));
 
         searchButton.setText("Søg");
+        searchButton.setPreferredSize(new Dimension(90, 25));
         searchButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 searchButtonActionPerformed(evt);
             }
         });
-        searchPanel.add(searchButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(690, 78, -1, -1));
 
         searchOriginLabel.setText("Udgangspunkt");
-        searchPanel.add(searchOriginLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 51, -1, -1));
+        searchPanel.add(searchOriginLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 47, -1, 25));
 
-        searchOriginComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[]{"All"}));
+        searchOriginComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[]{"Alle"}));
         searchOriginComboBox.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 searchOriginComboBoxActionPerformed(evt);
             }
         });
-        searchPanel.add(searchOriginComboBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(82, 47, 264, -1));
+        searchPanel.add(searchOriginComboBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(102, 47, 204, 25));
 
         searchDestinationLabel.setText("Destination");
-        searchPanel.add(searchDestinationLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 82, -1, -1));
+        searchPanel.add(searchDestinationLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 77, -1, 25));
 
-        searchDestinationComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[]{"All"}));
-        searchPanel.add(searchDestinationComboBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(82, 78, 264, -1));
-        searchPanel.add(searchDepartureTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 18, 255, -1));
+        searchDestinationComboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[]{"Alle"}));
+        searchPanel.add(searchDestinationComboBox, new org.netbeans.lib.awtextra.AbsoluteConstraints(102, 77, 204, 25));
 
-        searchDepartureDLabel.setText("Afgang");
-        searchPanel.add(searchDepartureDLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(358, 21, -1, -1));
 
-        searchArrivalLabel.setText("Ankomst");
-        searchPanel.add(searchArrivalLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(358, 51, -1, -1));
-        searchPanel.add(searchArrivalTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 48, 255, -1));
+        searchPanel.add(searchDepartureJXDatePicker, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 18, 195, 25));
+
+
+        searchDepartureDLabel.setText("Afgangs dato");
+        searchPanel.add(searchDepartureDLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(318, 18, -1, 25));
+
+        searchDepartureTimeLabel.setText("Afgangs tidspunkt");
+        searchPanel.add(searchDepartureTimeLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(318, 48, -1, 25));
+        searchPanel.add(searchDepartureTimeHourTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 48, 70, 25));
+        searchTimeSplitLetterDepartureLabel.setText(":");
+        searchPanel.add(searchTimeSplitLetterDepartureLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(536, 48, -1, 25));
+        searchPanel.add(searchDepartureTimeMinuteTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(565, 48, 70, 25));
+
+        searchArrivalLabel.setText("Ankomst dato");
+        searchPanel.add(searchArrivalLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(318, 77, -1, 25));
+        searchPanel.add(searchArrivalJXDatePicker, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 77, 195, 25));
+        searchArrivalTimeLabel.setText("Ankomst tidspunkt");
+        searchPanel.add(searchArrivalTimeLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(318, 107, -1, 25));
+        searchPanel.add(searchArrivalTimeHourTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 107, 70, 25));
+        searchTimeSplitLetterArrivalLabel.setText(":");
+        searchPanel.add(searchTimeSplitLetterArrivalLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(536, 107, -1, 25));
+        searchPanel.add(searchArrivalTimeMinuteTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(565, 107, 70, 25));
 
         searchMinSeatsLabel.setText("Antal fri sæder");
-        searchPanel.add(searchMinSeatsLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(358, 82, -1, -1));
+        searchPanel.add(searchMinSeatsLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(318, 136, -1, 25));
 
         searchMinSeatsTextField.setValue(new Double(0));
         searchMinSeatsTextField.setColumns(10);
-        searchPanel.add(searchMinSeatsTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(542, 79, 132, -1));
+        searchPanel.add(searchMinSeatsTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 136, 195, 25));
 
         clearSearchButton.setText("Ryd felter");
+        clearSearchButton.setPreferredSize(new Dimension(90, 25));
         clearSearchButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 clearSearchButtonActionPerformed(evt);
             }
         });
-        searchPanel.add(clearSearchButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 45, -1, -1));
-        searchPanel.setMinimumSize(new Dimension(775, 120));
+        searchPanel.add(clearSearchButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 107, -1, 25));
+        searchPanel.add(searchButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 136, -1, 25));
 
-        actionsPanel.setPreferredSize(new Dimension(165, 120));
+        actionsPanel.setPreferredSize(new Dimension(283, 175));
         DefaultTableModel tableModel = new DefaultTableModel(new Object[][]{},
                 new String[]{
             "ID", "Udgangspunkt", "Destination", "Afgang", "Ankomst", "Antal frie sæder"
@@ -216,19 +285,20 @@ public class FlightBrowser extends Browser {
         showReservationButton.setText("Vis reservationer for den valgte afgang");
         showReservationButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
+                if (flightTable.getSelectedRow()>-1)
                 activateActionPerformed(evt);
             }
         });
 
         actionsPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        
         returnToMainMenuButton.setText("Tilbage til hovedmenuen");
         returnToMainMenuButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 returnActionPerformed(evt);
             }
         });
-        actionsPanel.add(returnToMainMenuButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 78, -1, -1));
+        actionsPanel.add(showReservationButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 107, 260, 25));
+        actionsPanel.add(returnToMainMenuButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 136, 260, 25));
 
 
 
@@ -252,18 +322,22 @@ public class FlightBrowser extends Browser {
 
     private void clearSearchButtonActionPerformed(java.awt.event.ActionEvent evt) {
         // TODO add your handling code here:
-        searchFlightIDTextField.setText("All");
-        searchOriginComboBox.setSelectedIndex(-1);
-        searchDestinationComboBox.setSelectedIndex(-1);
-        searchDepartureTextField.setText("");
-        searchArrivalTextField.setText("");
+        searchFlightIDTextField.setText("");
+        searchOriginComboBox.setSelectedIndex(0);
+        searchDestinationComboBox.setSelectedIndex(0);
+        searchDepartureJXDatePicker.setDate(null);
+        searchArrivalJXDatePicker.setDate(null);
         searchMinSeatsTextField.setText("0");
+        searchArrivalTimeHourTextField.setText(searchArrivalTimeHourTextField.hint);
+        searchArrivalTimeMinuteTextField.setText(searchArrivalTimeMinuteTextField.hint);
+        searchDepartureTimeHourTextField.setText(searchDepartureTimeHourTextField.hint);
+        searchDepartureTimeMinuteTextField.setText(searchDepartureTimeMinuteTextField.hint);
+        UpdateTable();
     }
 
     private void searchOriginComboBoxActionPerformed(java.awt.event.ActionEvent evt) {
         // TODO add your handling code here:
     }
-
     // Variables declaration - do not modify                     
     private javax.swing.JButton clearSearchButton;
     protected javax.swing.JTable flightTable; //Hahahahahahahaha
@@ -271,10 +345,18 @@ public class FlightBrowser extends Browser {
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JButton returnToMainMenuButton;
     private javax.swing.JLabel searchArrivalLabel;
-    private javax.swing.JTextField searchArrivalTextField;
+    private JXDatePicker searchArrivalJXDatePicker;
+    private JXDatePicker searchDepartureJXDatePicker;
+    private javax.swing.JLabel searchArrivalTimeLabel;
+    private javax.swing.JLabel searchDepartureTimeLabel;
+    private javax.swing.JLabel searchTimeSplitLetterDepartureLabel;
+    private javax.swing.JLabel searchTimeSplitLetterArrivalLabel;
+    private external.HintTextField searchArrivalTimeHourTextField;
+    private external.HintTextField searchArrivalTimeMinuteTextField;
+    private external.HintTextField searchDepartureTimeHourTextField;
+    private external.HintTextField searchDepartureTimeMinuteTextField;
     private javax.swing.JButton searchButton;
     private javax.swing.JLabel searchDepartureDLabel;
-    private javax.swing.JTextField searchDepartureTextField;
     private javax.swing.JComboBox searchDestinationComboBox;
     private javax.swing.JLabel searchDestinationLabel;
     private javax.swing.JLabel searchFlightIDLabel;
--- GPP_Flyprojekt/src/classes/ReservationBrowser.java
+++ GPP_Flyprojekt/src/classes/ReservationBrowser.java
@@ -68,7 +68,7 @@ public class ReservationBrowser extends Browser {
 
     private void initComponents() {
         setTitle("Vælg en reservation");
-        setMinimumSize(new Dimension(875, 600));
+        setMinimumSize(new Dimension(1000, 600));
         titleLabel = new javax.swing.JLabel();
         searchPanel = new javax.swing.JPanel();
         searchReservationIDTextField = new javax.swing.JTextField();
@@ -98,43 +98,45 @@ public class ReservationBrowser extends Browser {
         searchPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Søg"));
         searchPanel.setToolTipText("");
         searchPanel.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        searchPanel.add(searchReservationIDTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 20, 240, -1));
+        searchPanel.add(searchReservationIDTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(122, 20, 195, 25));
 
         searchFlightIDLabel.setText("ReservationID");
-        searchPanel.add(searchFlightIDLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 23, -1, -1));
+        searchPanel.add(searchFlightIDLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 20, -1, 25));
 
         searchButton.setText("Søg");
+        searchButton.setPreferredSize(new Dimension(90,25));
         searchButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 searchButtonActionPerformed(evt);
             }
         });
-        searchPanel.add(searchButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 80, -1, -1));
-        searchPanel.add(searchPassengerNameTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 50, 240, -1));
+        searchPanel.add(searchButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(655, 80, -1, 25));
+        searchPanel.add(searchPassengerNameTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(122, 49, 195, 25));
 
         searchDepartureDLabel.setText("Navn på passager");
-        searchPanel.add(searchDepartureDLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 53, -1, -1));
+        searchPanel.add(searchDepartureDLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 49, -1, 25));
 
         searchArrivalLabel.setText("Telefonnummer");
-        searchPanel.add(searchArrivalLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 23, -1, -1));
-        searchPanel.add(searchTlfTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 20, 180, -1));
+        searchPanel.add(searchArrivalLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 20, -1, -25));
+        searchPanel.add(searchTlfTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(450, 20, 195, 25));
 
         searchMinSeatsLabel.setText("Antal passagerer");
-        searchPanel.add(searchMinSeatsLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 83, -1, -1));
+        searchPanel.add(searchMinSeatsLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(14, 78, -1, 25));
 
         searchTotalPassengersTextField.setText("0");
 
         searchTotalPassengersTextField.setValue(new Double(0));
         searchTotalPassengersTextField.setColumns(10);
-        searchPanel.add(searchTotalPassengersTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(218, 80, 132, -1));
+        searchPanel.add(searchTotalPassengersTextField, new org.netbeans.lib.awtextra.AbsoluteConstraints(122, 78, 195, 25));
 
         clearSearchButton.setText("Ryd felter");
+        clearSearchButton.setPreferredSize(new Dimension(90,25));
         clearSearchButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 clearSearchButtonActionPerformed(evt);
             }
         });
-        searchPanel.add(clearSearchButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 50, -1, -1));
+        searchPanel.add(clearSearchButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(655, 50, -1, 25));
         DefaultTableModel tableModel = new DefaultTableModel(new Object[][]{},
                 new String[]{
             "ID", "Navn på passager", "Antal passagerer", "Telefonnummer"
@@ -158,7 +160,7 @@ public class ReservationBrowser extends Browser {
         reservationTable.setModel(tableModel);
         reservationTable.setRequestFocusEnabled(false);
         jScrollPane2.setViewportView(reservationTable);
-
+        showReservationButton.setPreferredSize(new Dimension(210,25));
         showReservationButton.setText("Redigér den valgte reservation");
         showReservationButton.addActionListener(new ActionListener(){
             @Override
@@ -176,10 +178,12 @@ public class ReservationBrowser extends Browser {
                 returnActionPerformed(evt);
             }
         });
-        actionsPanel.add(returnToFlightBrowserButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 80, -1, -1));
-        searchPanel.setMinimumSize(new Dimension(655, 120));
+        actionsPanel.add(returnToFlightBrowserButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 80, -1, 25));
+        returnToFlightBrowserButton.setPreferredSize(new Dimension(210,25));
+        actionsPanel.add(showReservationButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 50, -1, 25));
+  
 
-        actionsPanel.setPreferredSize(new Dimension(185, 120));
+        actionsPanel.setPreferredSize(new Dimension(230, 120));
 
 
         JPanel buttonPane = new JPanel();
