ce1da68a7bab6a64e6fb30fb5cc6630df0808155
Rabka <smot@itu.dk>
2013-12-15 21:56:55 +0100

    By stinus - commentare til seating

--- GPP_Flyprojekt/src/classes/Seating.java
+++ GPP_Flyprojekt/src/classes/Seating.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
 
 /**
  *
@@ -19,7 +18,11 @@ public class Seating implements ISeating {
     private HashMap<Point, Boolean> isSeatFree;
     private HashMap<Point, Boolean> isSeatChosen;
     private AirplaneSeat[] seats;
-
+    
+    /**
+     * setting the seats for the flight, up in the respektable variabels. 
+     * @param flight 
+     */
     public Seating(IFlight flight) {
         seats = flight.getPlane().airplaneLayout.airplaneSeats;
         isSeatFree = new HashMap<Point, Boolean>();
@@ -42,6 +45,12 @@ public class Seating implements ISeating {
        
     }
     
+    /**
+     * a method to get the seats position, form the seats row,collen deskripion.
+     * @param seat ther is the row,collen deskripion of the sead.
+     * @return the aktual position of the seat.
+     * @throws IndexOutOfBoundsException if the seat parameter dose not deskripe a seat in seatPositions.
+     */
     @Override
     public Point getSeatPosition(Point seat) throws IndexOutOfBoundsException {
         if (seatPositions.keySet().contains(seat)) {
@@ -51,6 +60,12 @@ public class Seating implements ISeating {
         }
     }
     
+    /**
+     * a method to get if the seat is free, form the seats row,collen deskripion.
+     * @param seat ther is the row,collen deskripion of the sead.
+     * @return if the seat is free.
+     * @throws IndexOutOfBoundsException if the seat parameter dose not deskripe a seat in seatPositions.
+     */
     @Override
     public boolean getSeatFree(Point seat) throws IndexOutOfBoundsException {
         if (seatPositions.keySet().contains(seat)) {
@@ -60,6 +75,12 @@ public class Seating implements ISeating {
         }
     }
     
+    /**
+     * a method to get if the seat is choosen, form the seats row,collen deskripion.
+     * @param seat ther is the row,collen deskripion of the sead.
+     * @return if the seat is choosen.
+     * @throws IndexOutOfBoundsException if the seat parameter dose not deskripe a seat in seatPositions.
+     */
     @Override
     public boolean getSeatChosen(Point seat) throws IndexOutOfBoundsException {
         if (seatPositions.keySet().contains(seat)) {
@@ -68,12 +89,21 @@ public class Seating implements ISeating {
             throw new IndexOutOfBoundsException("No seat at the given placement: " + seat.toString());
         }
     }
-     
+    
+    /**
+     * a method to test if at seat exist with the given row,collen deskripion.  
+     * @param seat the given row,collen deskripion of the seat.
+     * @return if ther is a seat with the given row,collen deskripion in seatPositions.
+     */
     @Override
     public boolean getSeatExists(Point seat) {
         return seatPositions.keySet().contains(seat);
     }
-
+    
+    /**
+     * a method to make a iterator over the seats row,collen deskripion in seatPositions without giving access to the variable.
+     * @return a clone of the keys in seatPositions.
+     */
     @Override
     public Iterator<Point> getSeatIterator() {
         HashSet<Point> keySet = new HashSet<Point>();
@@ -84,13 +114,21 @@ public class Seating implements ISeating {
     
     // --- the old ISeatingHandler ---
     // the next methos use the privios methos to giv data in better ways
-
+    
+    /**
+     * a method to get the number of free seats.
+     * @return the number of free seats.
+     */
     @Override
     public int getNumberOfFreeSeats()
     {
         return getFreeSeats().size();
     }
-
+    
+    /**
+     * a method to set if at seat is to be choosen.
+     * @param seat the row,collen deskripion of the seat to be choosen.
+     */
     @Override
     public void setChosen(Point seat)
     {
@@ -98,13 +136,21 @@ public class Seating implements ISeating {
             isSeatChosen.put(seat,true);
     }
     
+    /**
+     * a method to set if at seat is not to be choosen.
+     * @param seat the row,collen deskripion of the seat to be unchoosen.
+     */
     @Override
     public void removeChosen(Point seat)
     {
         if(getSeatChosen(seat))
             isSeatChosen.put(seat,false);
     }
-
+    
+    /**
+     * a method to make a arraylist of seats choosen insteat of not free.
+     * @param seats the arraylist of the row,collen deskripion seats to be choosen.
+     */
     @Override
     public void changeTakenToChosen(ArrayList<Point> seats)
     {
@@ -117,7 +163,11 @@ public class Seating implements ISeating {
             isSeatChosen.put(seat,true);
         }
     }
-
+    
+    /**
+     * a method to get all not free seats.
+     * @return a arraylist of the row,collen deskripion seats ther is not free.
+     */
     @Override
     public ArrayList<Point> getTakenSeats()
     {
@@ -132,7 +182,11 @@ public class Seating implements ISeating {
         
         return takenSeats;
     }
-
+    
+    /**
+     * a method to get all free seats.
+     * @return a arraylist of the row,collen deskripion seats ther is free.
+     */
     @Override
     public ArrayList<Point> getFreeSeats()
     {
@@ -146,19 +200,16 @@ public class Seating implements ISeating {
         }
         return freeSeats;
     }
-
+    
+    /**
+     * a method to get all choosen seats.
+     * @return a arraylist of the row,collen deskripion seats ther is choosen.
+     */
     @Override
     public ArrayList<Point> getChosenSeats()
     {
         ArrayList<Point> chosenSeats = new ArrayList<Point>();
         Iterator<Point> iter = getSeatIterator();
-        
-        /*System.out.println("Testing Iterator:");
-        Iterator<Point> iter2 = getSeatIterator();
-        while (iter2.hasNext()) {
-            System.out.println("- "+iter2.next());
-        }
-        System.out.println("DONE");*/// <TODO> permanent fixing the (1, 0) => (-1, 0) error 
         
         while(iter.hasNext())
         {
